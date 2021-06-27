795b8298841046726bbd0abf65b4b2a45e63d56c
Novilawati <novilawati@kompas.com>
2021-06-27 12:45:11 +0800

    first commit

---
+++ _loader.php
@@ -0,0 +1,7 @@
+<?php
+$_dir=__DIR__;
+define('env',$_dir);
+include(env.'/env.php');
+
+//helper
+include '_helpers/helper.php';
---
+++ env.php
@@ -0,0 +1,18 @@
+<?php
+defined('env') OR exit('Akses langsung ke Skrip ini diblokir');
+
+$setDb['db_host']='127.0.0.1';
+$setDb['db_name']='';
+$setDb['db_user']='root';
+$setDb['db_password']='';
+
+//folder templates
+$template='templates/AdminLTE-2.4.17/';
+
+//session
+$setSession['prefix']='sip';
+
+//URI
+$setUri['base']='http://sip.sha-dev.com/';
+$setUri['assets']='assets/';
+?>
