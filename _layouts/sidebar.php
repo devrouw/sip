928a6677d9d2e1166c38d3efa9bd3744c71b4fbb
Novilawati <novilawati@kompas.com>
2021-06-29 22:09:53 +0800

    login session

--- _layouts/sidebar.php
+++ _layouts/sidebar.php
@@ -1,3 +1,4 @@
+<!-- Left side column. contains the sidebar -->
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
@@ -7,7 +8,7 @@
           <img src="<?=templates()?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Alexander Pierce</p>
+          <p>Hello</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
@@ -25,53 +26,50 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="treeview">
+        <li>
           <a href="<?=url('beranda')?>">
             <i class="fa fa-dashboard"></i> <span>Beranda</span>
           </a>
         </li>
-        <li class="treeview">
-          <a href="<?=url('kecamatan')?>">
-            <i class="fa fa-map-marker"></i> <span>Kecamatan</span>
-          </a>
-        </li>
+        <li><a href="<?=url('kecamatan')?>"><i class="fa fa-circle-o"></i> Kecamatan</a></li>
+
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
-            <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="../layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="../layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="../layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="../layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="../widgets.html">
-            <i class="fa fa-th"></i> <span>Widgets</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-green">Hot</small>
-            </span>
-          </a>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
+            <i class="fa fa-sitemap"></i>
+            <span>Transaksi</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="../charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="../charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="../charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+            <li><a href="<?=url('hotspot')?>"><i class="fa fa-circle-o"></i> Hotspot</a></li>
           </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-map"></i>
+            <span>Leaflet</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="<?=url('leaflet-standar')?>"><i class="fa fa-circle-o"></i> Standar</a></li>
+            <li><a href="<?=url('leaflet-point')?>"><i class="fa fa-circle-o"></i> Point</a></li>
+            <li><a href="<?=url('leaflet-pointmarker')?>"><i class="fa fa-circle-o"></i> Point Marker</a></li>
+            <li><a href="<?=url('leaflet-pointgeojson')?>"><i class="fa fa-circle-o"></i> Point Marker GeoJSON</a></li>
+            <li><a href="<?=url('leaflet-cluster')?>"><i class="fa fa-circle-o"></i> Cluster</a></li>
+            <li><a href="<?=url('leaflet-clustercorona')?>"><i class="fa fa-circle-o"></i> Cluster Corona</a></li>
+            <li><a href="<?=url('leaflet-heatmap')?>"><i class="fa fa-circle-o"></i> Heatmap</a></li>
+            <li><a href="<?=url('leaflet-choroplet')?>"><i class="fa fa-circle-o"></i> Choroplet</a></li>
+            <li><a href="<?=url('leaflet-routingmachine')?>"><i class="fa fa-circle-o"></i> Routing Machine</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="<?=url('logout')?>">
+            <i class="fa fa-sign-out"></i> <span>Keluar</span>
+          </a>
         </li>
       </ul>
     </section>
