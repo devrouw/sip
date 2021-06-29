924f26b82e45f53611358b0df1e41e1ba286016f
Novilawati <novilawati@kompas.com>
2021-06-29 21:14:11 +0800

    login session

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -1,6 +1,7 @@
 <?php
 $title = 'Beranda';
 $judul = $title;
+$setTemplate = true;
 ?>
 <?= content_open('Halaman Beranda') ?>
 Selamat Datang di Beranda
--- index.php
+++ index.php
@@ -15,47 +15,48 @@ if (!file_exists($file)) {
 $content = ob_get_contents();
 ob_end_clean();
 
-if($setTemplate==true){
-    if($session->get("logged")!=true){
+if ($setTemplate == true) {
+    if ($session->get("logged") != true) {
         redirect(url('login'));
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<?php include '_layouts/head.php' ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <?php include '_layouts/head.php' ?>
 
-<body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-        <?php
-        include '_layouts/header.php';
-        include '_layouts/sidebar.php';
-        ?>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            <?php
+            include '_layouts/header.php';
+            include '_layouts/sidebar.php';
+            ?>
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    <?=$judul?>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                    <li class="active"><?=$judul?></li>
-                </ol>
-            </section>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        <?= $judul ?>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li class="active"><?= $judul ?></li>
+                    </ol>
+                </section>
 
-            <?php echo $content; ?>
-            <!-- /.content -->
+                <?php echo $content; ?>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+            <?php
+            include '_layouts/footer.php';
+            include '_layouts/javascript.php';
+            ?>
         </div>
-        <!-- /.content-wrapper -->
-        <?php
-        include '_layouts/footer.php';
-        include '_layouts/javascript.php';
-        ?>
-    </div>
-</body>
-</html>
+    </body>
+
+    </html>
 <?php } else {
     echo $content;
-}?>
+} ?>
