b30e45b16b224f11ade5b276c8e5febf5b6d3d65
Novilawati <novilawati@kompas.com>
2021-07-06 17:32:41 +0800

    change bangunan form style

--- _halaman/bangunan.php
+++ _halaman/bangunan.php
@@ -86,10 +86,13 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
     <?= content_open('Form Data Bangunan') ?>
     <form method="post" enctype="multipart/form-data">
         <?= input_hidden('id_bangunan', $id_bangunan) ?>
+        <div class="row">
+        <div class="col-md-6">
+        
         <div class="form-group" class="">
             <label>Foto Bangunan</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_file('foto_bangunan', $foto_bangunan) ?>
             </div>
             </div>
@@ -97,7 +100,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Jenis Bangunan</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('jenis_bangunan', $jenis_bangunan) ?>
             </div>
             </div>
@@ -105,7 +108,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Alamat</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('alamat', $alamat) ?>
             </div>
             </div>
@@ -113,7 +116,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Longitude</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('lng', $lng) ?>
             </div>
             </div>
@@ -121,7 +124,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Latitude</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('lat', $lat) ?>
             </div>
             </div>
@@ -129,7 +132,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Nomor Rumah</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('nomor_rumah', $nomor_rumah) ?>
             </div>
             </div>
@@ -137,7 +140,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Luas Tanah</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('luas_tanah', $luas_tanah) ?>
             </div>
             </div>
@@ -145,7 +148,7 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Luas Bangunan</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?= input_text('luas_bangunan', $luas_bangunan) ?>
             </div>
             </div>
@@ -153,20 +156,24 @@ if (isset($_GET['tambah']) or isset($_GET['ubah'])) {
         <div class="form-group">
             <label>Akun Warga</label>
             <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
             <?php
 	    		$op['']='Pilih Akun Warga';
 	    		foreach ($db->ObjectBuilder()->get('tb_warga') as $row) {
 	    			$op[$row->id_warga]=$row->username;
-	    			}
+	    		}
 	    	?>
 	    		<?=select('id_warga',$op,$id_warga)?>
             </div>
             </div>
         </div>
+        </div>
+        <div class="col-md-12">
         <div class="form-group">
             <button type="submit" name="simpan" class="btn btn-info"> <i class="fa fa-save"></i>Simpan</button>
             <a href="<?= url($url) ?>" class="btn btn-danger"> <i class="fa fa-reply"></i>Kembali</a>
+        </div>
+        </div>
         </div>
     </form>
     <?= content_close() ?>
